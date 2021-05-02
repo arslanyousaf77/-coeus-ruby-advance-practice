e488fcc94ad05d55b33ff22d87757949658bb43e
Arslan Yousaf <arslanyousaf77@gmail.com>
2021-05-03 00:36:44 +0500

    oop

--- oop.rb
+++ oop.rb
@@ -1,9 +1,21 @@
 class Box
     # constructor method
+    @@count = 0
+
+    BOX_COMPANY = "Coeus Solutions GmbH"
+    BOXWEIGHT = 10
+    
     def initialize(w,h)
+       # assign instance avriables
        @width, @height = w, h
+ 
+       @@count += 1
     end
  
+    def self.printCount()
+       puts "Box count is : #@@count"
+    end
+
     # accessor methods
     def getWidth
        @width
@@ -12,6 +24,7 @@ class Box
        @height
     end
  
+    private :getHeight, :getWidth
     # setter methods
     def setWidth=(value)
        @width = value
@@ -24,20 +37,83 @@ class Box
     def getArea
     @width * @height
     end
+
+    def printArea
+        @area = getWidth() * getHeight
+        puts "Big box area is : #@area"
+     end
+     protected :printArea
+     
+    def to_s
+        "(w:#@width,h:#@height)"  # string formatting of the object.
+    end
+
+    #Addition of two Box Objects
+    def +(other)
+        Box.new(@width+other.width,@height+other.height)
+    end
+
+    #Negation of two objects
+    def -@
+        Box.new(-@width, -@height)
+    end
+
+    #Scalar Multiplication
+    def *(scalarNum)
+        Box.new(@width*scalarNum, @height*scalarNum)
+    end
+
+
  end
  
  # create an object
  box = Box.new(10, 20)
+ box2 = Box.new(12,2)
  
  # use setter methods
  box.setWidth = 30
  box.setHeight = 50
  
  # use accessor methods
- x = box.getWidth()
- y = box.getHeight()
+
  a = box.getArea()
  
- puts "Width of the box is : #{x}"
- puts "Height of the box is : #{y}"
  puts "Area of the box is : #{a}"
+ Box.printCount()
+ puts "String representation of box is : #{box}"
+ #box.printArea
+
+ puts "String representation of box is : #{-box}"
+
+ box.freeze
+if( box.frozen? )
+   puts "Box object is frozen object"
+else
+   puts "Box object is normal object"
+end
+
+# now try using setter methods
+# box.setWidth = 30
+# box.setHeight = 50
+
+puts Box::BOX_COMPANY
+puts "Box weight is: #{Box::BOXWEIGHT}"
+
+# create another object using allocate
+box2 = Box.allocate
+
+
+ class BigBox < Box
+
+    # add a new instance method
+    def printArea
+       @area = @width * @height
+       puts "Big box area is : #@area"
+    end
+ end
+ 
+ # create an object
+ box = BigBox.new(10, 20)
+ 
+ # print the area
+ box.printArea()
